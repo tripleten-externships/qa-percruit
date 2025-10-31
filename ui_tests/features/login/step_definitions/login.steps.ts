05b54c21af8260653cc0b09a6c5b9734b8038d34
remyaanish <remya.agnus@gmail.com>
2025-10-31 11:50:07 -0400

    Changes to support headed run

--- ui_tests/features/login/step_definitions/login.steps.ts
+++ ui_tests/features/login/step_definitions/login.steps.ts
@@ -8,32 +8,25 @@ import { chromium, Browser, Page, expect } from '@playwright/test';
 
 // Import environment configuration and Page Object Models
 import * as env from '../../../src/config/world';
+import { CustomWorld } from '../../../src/config/world';
 import { LoginPage } from '../../../src/pages/common/LoginPage';
 import { StudentDashboardPage } from '../../../src/pages/student/StudentDashboardPage';
 
 // Declare variables to hold browser, page, and page object instances
-let page: Page;
-let browser: Browser;
 let loginPage: LoginPage;
 let studentDashboardPage: StudentDashboardPage;
 
 // Before hook: Launch a new browser and page before each scenario and initialize page objects
-Before(async () => {
-  browser = await chromium.launch();
-  page = await browser.newPage();
-  loginPage = new LoginPage(page);
-  studentDashboardPage = new StudentDashboardPage(page);
+Before(async function() {
+  loginPage = new LoginPage(this.page);
+  studentDashboardPage = new StudentDashboardPage(this.page);
 });
 
-// After hook: Close the browser after each scenario
-After(async () => {
-  await browser.close();
-});
 
 // Step: Navigate to the Percruit homepage
-Given('I am on the Percruit homepage', async () => {
-  await page.goto(env.getBaseUrl());
-  await expect(page).toHaveURL(/percruit.com/);
+Given('I am on the Percruit homepage', async function() {
+  await this.page.goto(env.getBaseUrl());
+  await expect(this.page).toHaveURL(/percruit.com/);
 });
 
 // Step: Log in as a specific user type (Student, Admin, Mentor)
@@ -45,16 +38,16 @@ When(
 );
 
 // Step: Login as a specific user type (Student, Admin, Mentor)
-When(/the (.+) is authenticated in the system/, async (userType) => {
-  await page.goto(env.getBaseUrl());
-  await expect(page).toHaveURL(/percruit.com/);
+When(/the (.+) is authenticated in the system/, async function (userType) {
+  await this.page.goto(env.getBaseUrl());
+  await loginPage.waitForPageLoad();
   await loginPage.loginAsUserType(userType);
 });
 
 // Step: Verify that the Student is on the Student Dashboard
-Then('the Student should be able to see the Student Dashboard', async () => {
-  await expect(page).toHaveURL(/dashboard/);
-  // await page.screenshot({ path: 'screenshots/student-dashboard.png', fullPage: true });
+Then('the Student should be able to see the Student Dashboard', async function() {
+  await studentDashboardPage.waitForPageLoad();
+  await expect(this.page).toHaveURL(/dashboard/);
   const isVisible = await studentDashboardPage.isOnDashboardPage();
   expect(isVisible).toBeTruthy();
 });
