26b8af078ffb680bd15b345676bd2f81d8418016
anshumatomar <104050926+anshumatomar@users.noreply.github.com>
2025-11-04 02:24:32 +0000

    step_definitions files added

---
+++ ui_tests/features/master-list/usage-metrics/step_definitions/realtimeactivity.steps.ts
@@ -0,0 +1,35 @@
+// Step definitions for login feature using Cucumber and Playwright
+
+// Import Cucumber hooks and step definition decorators
+import { Given, When, Then, Before, After } from '@cucumber/cucumber';
+
+// Import Playwright classes and assertion utilities
+import { chromium, Browser, Page, expect } from '@playwright/test';
+
+// Import environment configuration and Page Object Models
+import * as env from '../../../../src/config/world';
+import { LoginPage } from '../../../../src/pages/common/LoginPage';
+import { StudentDashboardPage } from '../../../../src/pages/student/StudentDashboardPage';
+ 
+        Given('the user is on Admin Dashboard page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+
+         When('the user click on the Usage Metrics tab', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+
+         When('apply the {string} time filter', function (string) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+       
+         Then('user should see the usage counts updated for {string}', function (string) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
