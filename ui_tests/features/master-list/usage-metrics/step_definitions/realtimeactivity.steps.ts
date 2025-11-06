58369137939e80608a6468e15b27290cf49bde05
Anshuma <anshuma.tomar@gmail.com>
2025-11-05 17:24:21 -0800

    steps.ts modified

--- ui_tests/features/master-list/usage-metrics/step_definitions/realtimeactivity.steps.ts
+++ ui_tests/features/master-list/usage-metrics/step_definitions/realtimeactivity.steps.ts
@@ -8,20 +8,26 @@ import * as env from '../../../../src/config/world';
 import { LoginPage } from '../../../../src/pages/common/LoginPage';
 
 
-Given('the user is on the Admin Dashboard page', async function () {
+Before(async function () {
+  // Initialize the EventsPage object with the current browser page context
+
+});
+
+Given('the user on the Admin Dashboard page', async ({ page }) => {
  // Write code here that turns the phrase above into concrete actions
- await this.page.getByRole('button',{name:'Usage Metrics'}).click();
- await expect(this.page.getByRole('heading',{name:'Admin Usage Metrics'})).toBeVisible();
+  await page.getByRole('button', { name: 'ANALYTICS & REPORTING' }).click();
+  await page.getByRole('button', { name: 'Usage Metrics' }).click();
+  await expect(page.getByRole('heading', { name: 'Admin Usage Metrics' })).toBeVisible();
 });        
        
 
-When('the user click on the Usage Metrics tab', function (string) {
+When('the user click on the {string} tab', function (string) {
  // Write code here that turns the phrase above into concrete actions
  return 'pending';
 });
        
 
- When('the user click on the Real-time Activity tab', function (string) {
+ When('the user click on the {string} tab', function (string) {
  // Write code here that turns the phrase above into concrete actions
  return 'pending';
 });
