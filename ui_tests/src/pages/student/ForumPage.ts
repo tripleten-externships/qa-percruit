c4458c0ccaa23464554f24819bbdffdd00e28d85
Gkaur643 <gurpinder.kaur3090@gmail.com>
2025-12-01 14:14:55 -0800

    forumpage changes

--- ui_tests/src/pages/student/ForumPage.ts
+++ ui_tests/src/pages/student/ForumPage.ts
@@ -3,17 +3,16 @@ import { BasePage } from '../common/BasePage';
 
 export class ForumPage extends BasePage {
 
-    IndustryNewsHeading = '//h1[text() = "Industry News"]';
-    SearchEditBox = '//input[@placeholder="Search articles..."]';
-    TitleInputBox= '//input[@placeholder=" What\'s your topic about?"]';
+    ForumsHeading = '//h5[text() = "Forums"]';
+    SearchEditBox = '//input[@placeholder="Search posts..."]';
+     SortByEditBox= '//input[@placeholder=" Filter by Topic"]';
    
-    SortByEditBox = '//input[@value="relevance"]';
   constructor(page: Page) {
     super(page);
   }
 
   async verifyPage(){
-    await expect(this.page.locator(this.IndustryNewsHeading)).toBeVisible();
+    await expect(this.page.locator(this.ForumsHeading)).toBeVisible();
     await expect(this.page.locator(this.SearchEditBox)).toBeVisible();
     await expect(this.page.locator(this.SortByEditBox)).toBeVisible();
     
