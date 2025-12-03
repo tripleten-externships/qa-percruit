87c3bd1ac37ba7fe4d9926516cdff13f3af82ece
qaharvest <theqaharvest@gmail.com>
2025-12-02 19:20:43 -0500

    Test: Worked on load test case

---
+++ ui_tests/features/master-list/dashboard/step_definitions/load.steps.ts
@@ -0,0 +1,8 @@
+// Step definitions for login feature using Cucumber and Playwright
+
+// Import Cucumber hooks and step definition decorators
+import { Given, When, Then, Before, After } from '@cucumber/cucumber';
+
+// Import Playwright classes and assertion utilities
+import { chromium, Browser, Page, expect } from '@playwright/test';
+
