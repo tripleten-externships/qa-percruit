ac4df4e3da7cc5236e27bc61c9d9a2d7a8270743
Margaret Faye Maloloy-on <margaretfaye1218@gmail.com>
2025-12-04 13:31:06 -0500

    changing code again

--- ui_tests/features/student-interviews/step_definitions/interview-prep.steps.ts
+++ ui_tests/features/student-interviews/step_definitions/interview-prep.steps.ts
@@ -19,7 +19,7 @@ Before(async function() {
 });
 
 When('the user navigates to the Interview prep page', async function() {
-    await this.page.goto(env.getBaseUrl() + '/interview-prep');
+    await this.page.goto(`${env.getBaseUrl()}interview-prep`);
     await this.page.waitForLoadState('networkidle');
     await expect(this.page).toHaveURL('/interview-prep/');
 });
