f2fe54f8275f19d1e6361a79c341ba17e2be984a
tonster117 <tonster117@gmail.com>
2025-12-06 00:05:49 -0600

    Minor changes made

--- ui_tests/features/master-list/coding-problems/step_definitions/admin-coding-problems-activities.ts
+++ ui_tests/features/master-list/coding-problems/step_definitions/admin-coding-problems-activities.ts
@@ -1,10 +1,13 @@
 import { Given, Then, When} from "@cucumber/cucumber"
 import { Page } from "playwright";
+import {expect} from '@playwright/test'
+import { env } from "process";
 declare const page: Page;
 
         
     Given ('The admin is logged in using valid credentials and The admin is on the activites tab in coding problems', async function () {
-        await page.goto('https://stage.tripleten.percruit.com/');
+        //Login and navigate to "coding problems" page
+        await this.page.goto(env.BASE_URL);
         await page.getByRole('textbox', { name: 'Enter your email' }).click();
         await page.getByRole('textbox', { name: 'Enter your email' }).fill('qa+100119@qaexternship.testinator.com');
         await page.getByRole('textbox', { name: 'Enter your password' }).click();
@@ -13,15 +16,17 @@ declare const page: Page;
         
     });
         When ('The admin views the activity tab in coding problems', async function(){
+            //selects the activities tab
             await page.getByRole('tab', { name: 'Activities' }).click();
         });
 
         Then ('the admin should see all existing activities', async function(){
-
+            //page should be devoice of activities therefore no locator is needed. I simply added the url
+            await expect(page).toHaveURL('https://stage.tripleten.percruit.com/admin/coding-problems');
         });
 
    /* Given ('The admin is logged in using valid credentials and The admin is on the activites tab in coding problems', async function () {
-        await page.goto('https://stage.tripleten.percruit.com/');
+        await this.page.goto(env.BASE_URL);
         await page.getByRole('textbox', { name: 'Enter your email' }).click();
         await page.getByRole('textbox', { name: 'Enter your email' }).fill('qa+100119@qaexternship.testinator.com');
         await page.getByRole('textbox', { name: 'Enter your password' }).click();
@@ -32,7 +37,7 @@ declare const page: Page;
         When ('the admin filters a category using the category dropdown', async function(){
 
         });
-        Then ('the admin should be abvle to select a category fro mthe existing category dropdown', async function(){
+        Then ('the admin should be abvle to select a category from the existing category dropdown', async function(){
 
         });
 
