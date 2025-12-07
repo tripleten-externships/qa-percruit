23d0906cd317c5b26b5a69080f23ffe355432add
CheyanneDarby <Cheyannejaileen16@gmail.com>
2025-12-06 23:06:04 -0800

    Work in progress on logout feature

--- ui_tests/features/master-list/login/step_definitions/logout.steps.ts
+++ ui_tests/features/master-list/login/step_definitions/logout.steps.ts
@@ -1,28 +1,37 @@
 import { Given, Then, When } from "@cucumber/cucumber";
+import { Page } from 'playwright';
+declare const page: Page;
 
-Given('the admin is on the home page', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-       
-         Given('the admin is logged into the system', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
+        Given('the admin is on the home page', async function () {
+            await page.goto('https://stage.tripleten.percruit.com/'),
+            await page.getByRole('textbox', { name: 'Enter your email' }).click(),
+            await page.getByRole('textbox', { name: 'Enter your email' }).fill('cheyannejaileen16+admin@gmail.com'),
+            await page.getByRole('textbox', { name: 'Enter your password' }).click(),
+            await page.getByRole('textbox', { name: 'Enter your password' }).fill('Externship22');
+            await page.getByRole('button', { name: 'Sign In' }).click();
          });
        
   
-         When('the admin initiates a logout', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
+         When('the admin initiates a logout', async function () {
+            await page.locator('#root').getByText('Cheyanne Martinez-Darbycheyannejaileen16+admin@gmail.com').click();
+            await page.getByRole('menuitem', { name: 'Sign Out' }).click();
+           
          });
        
-         Then('the admin should be signed out successfully', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
+         Then('the admin should be signed out successfully', async function () {
+           
          });
        
        
-         Then('the login page should be displayed', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
+         Then('the login page should be displayed', async function () {
+           
+            await page.getByRole('heading', { name: 'Welcome to Percruit' }).click();
          });
+         
+        
+        
+         
+         
+        
+         
+         
