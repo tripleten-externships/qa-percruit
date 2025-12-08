eea7fdf6be4a66c602c3db017c179d6a803d96dc
Talise Hendrix <trhendri1@gmail.com>
2025-12-08 05:13:29 -0600

    test: add Current and Target Role on Restart Validation Scenarios

--- ui_tests/features/master-list/career-path/step_definitions/career-path.steps.ts
+++ ui_tests/features/master-list/career-path/step_definitions/career-path.steps.ts
@@ -47,54 +47,38 @@ Given('the student navigates to the Career Path page', async function () {
 });
 
 Given('the student submits the assessment with valid required details', async function () {
-
-    await expect(this.page).toHaveTitle(/Career Path Advisor/);
-    console.log("On Career Path TITLE Confirm");
-    // await this.page.waitForLoadState('networkidle');
+  console.log('1: Starting Assessment Section');
+ 
+ console.log('1.5:Wait for Page Load Successful');
+await expect(this.page).toHaveTitle(/Career Path Advisor/);
+console.log("2:On Career Path TITLE Confirm");
+  await this.page.waitForTimeout(5000);
+  console.log("Waited for load state Idle");
 // await this.page.getByPlaceholder('Select from list or type your own').click();
-await this.page.getByPlaceholder(careerPathPage.CurrentRoleTextBox).click();
-console.log("Clicked textbox");
-await this.page.getByPlaceholder(careerPathPage.CurrentRoleTextBox).fill("Student");
-console.log("Filled in current role");
-await this.page.locator(careerPathPage.ContinueButton).click({timeout:3000});
-console.log("Clicked continue button 1");
-await this.page.waitForTimeout(2000);
-// await this.page.locator(careerPathPage.TargetRoleTextBox).click();
-await this.page.locator('h6:has-text("Target Position")').locator('xpath=following::input[@placeholder="Select from list or type your own"][1]').fill('QA Engineer');
-// await this.page.getByLabel('Target Role*').click();
-// console.log("Clicked target role box");
-// await this.page.locator(careerPathPage.TargetRoleTextBox).fill('Qa Engineer');
-console.log("Filled in QA Eng");
-await this.page.locator(careerPathPage.ContinueButton).click({timeout:3000});
-console.log("Clicked continue button 2");
- await this.page.locator(careerPathPage.CompleteAssessmentButton).click();
-    console.log("clicked complete Assessment button");
+await this.page.locator('#_r_13_').click({timeout:5000});
+console.log("3:Clicked Current Role textbox");
+await this.page.locator('#_r_13_').fill("Student");
+console.log("4:Filled in current role");
+await this.page.locator(careerPathPage.ContinueButton).click({timeout:1000});
+console.log("5:Clicked continue button 1");
 
 
-    // await expect(this.page.locator(careerPathPage.CurrentPositionHeading)).toBeVisible();
-    // console.log("title is visible now");
-
-    // await this.page.locator(careerPathPage.CurrentRoleTextBox).click();
-    //  console.log("Clicked current role box");    
-  //   // await expect(this.page.getByPlaceholder(careerPathPage.CurrentRoleTextBox).toBeVisible());
-  // //  console.log("Current role box is visible");
-  //   await this.page.getByPlaceholder(careerPathPage.CurrentRoleTextBox).click();
-  //   console.log("Clicked current role box");
-  //   await this.page.locator(careerPathPage.CurrentRoleTextBox).fill("Student");
-  //   console.log("filled in 'student");
-  //   await this.page.locator(careerPathPage.ContinueButton).click({ timeout: 10000 });
-  //   console.log("Clicked continue button 1");
-  //   await this.page.locator(careerPathPage.TargetRoleTextBox).fill('Qa Engineer');
-  //   console.log("Filled in QA Eng");
-  //   await this.page.locator(careerPathPage.ContinueButton).click({ timeout: 10000 });
-  //   console.log("Clicked continue button 2");
-  //   await this.page.locator(careerPathPage.CompleteAssessmentButton).click();
-  //   console.log("clicked complete Assessment button");
+await this.page.locator('#_r_1a_').click();
+console.log("Clicked target role box");
+await this.page.locator('#_r_1a_').fill('Qa Engineer');
+console.log("6: Filled in QA Eng");
+///
+// await this.page.getByText('Continue', { exact: true }).isVisible();
+// await this.page.locator('(//button[normalize-space()="Continue"])[2]').click();
+await this.page.getByText('Continue').click()
+console.log("7: Clicked continue button 2");
+ await this.page.locator(careerPathPage.CompleteAssessmentButton).click({timeout:5000});
+    console.log("8: Clicked complete Assessment button");
     
- });
+  });
 
 When('the student clicks the restart button', async function () {
-  await this.page.locator(careerPathPage.RestartButton).click();
+  await this.page.locator(careerPathPage.RestartButton).click({timeout:5000});
   console.log("Clicked Restart Button");
   
 });
@@ -103,14 +87,14 @@ When('the student clicks the restart button', async function () {
 Then('Current Role displays Not specified', async function () {
   await this.page.waitForTimeout(15000);
   console.log("Getting Current Role Text");
-  let currentroletext = await this.page.locator(careerPathPage.CurrentRoleHeading).textContent();
+  let currentRoleText = await this.page.locator(careerPathPage.CurrentRoleHeading).textContent();
   await expect(this.page.locator(careerPathPage.CurrentRoleHeading)).toHaveText(/Not specified/);
-  console.log(currentroletext);
+  console.log(currentRoleText);
   console.log("Verified Current Role displays Not specified");
     
 });
 
-// Then('Target Role displays Not specified', async function () {
-//   await expect(this.page.locator(careerPathPage.TargetRoleHeading)).toHaveText(/Not specified/);
-//   console.log("Verified Target Role displays Not specified");
-// });
+Then('Target Role displays Not specified', async function () {
+  await expect(this.page.locator(careerPathPage.TargetRoleHeading)).toHaveText(/Not specified/);
+  console.log("Verified Target Role displays Not specified");
+});
