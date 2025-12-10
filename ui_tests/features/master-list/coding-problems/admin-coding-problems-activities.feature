200b394620f57b4cba7b8fa2d1cae4c40f3e367a
tonster117 <tonster117@gmail.com>
2025-12-09 23:17:23 -0600

    90% of tests completed

--- ui_tests/features/master-list/coding-problems/admin-coding-problems-activities.feature
+++ ui_tests/features/master-list/coding-problems/admin-coding-problems-activities.feature
@@ -7,29 +7,29 @@ Background:
   Given The Admin is logged in using valid credentials
   And the Admin is on the Activities tab in Coding Problems
 
-@indev
+@wip
 Scenario: View existing activities
   When the Admin views the Activities tab in Coding Problems
   Then the Admin should see all existing activities
   
- @indev
+ @wip
 Scenario: Selecting a Category on activities page
   When the Admin filters a category using the Category dropdown
   Then the Admin should be able to select a category from the existing Category dropdown list
 
-@indev
+@wip
 Scenario: Selecting a Topic on activities page
   When the Admin wants to select a new topic from the existing Topic dropdown list
   And a Category is already selected 
   Then the Admin should be able to select a topic from the Topic dropdown list 
 
-@indev
+@wip
 Scenario: Selecting a Unit on activities page
   When the Admin wants to select a new unit from the existing Unit dropdown list
   And a Topic is already selected
   Then the Admin should be able to select a unit from the Unit dropdown list
 
-@indev
+@wip
 Scenario: Adding an Activity
   When the Admin wants to add a new activity
   And the Admin selects a category from the existing Category dropdown list
@@ -38,7 +38,7 @@ Scenario: Adding an Activity
   Then the Admin should be able to add an activity with the valid information
   And the added activity should be visible to the Admin on the Activities page
 
-@indev
+@wip
 Scenario: Updating an existing Activity
   When the Admin makes an edit to an existing activity with valid information
   Then the Admin should be able to save the changes in the existing activity
--- ui_tests/features/master-list/coding-problems/step_definitions/admin-coding-problems-activities.steps.ts
+++ ui_tests/features/master-list/coding-problems/step_definitions/admin-coding-problems-activities.steps.ts
@@ -5,118 +5,142 @@ import { env } from "process";
 declare const page: Page;
 
 //scenario View existing activities        
-    Given ('The admin is logged in using valid credentials and The admin is on the activites tab in coding problems', async function () {
+    Given ('The Admin is logged in using valid credentials', async function () {
         //Login and navigate to "coding problems" page
         await this.page.goto(env.BASE_URL);
-        await page.getByRole('textbox', { name: 'Enter your email' }).click();
-        await page.getByRole('textbox', { name: 'Enter your email' }).fill('qa+100119@qaexternship.testinator.com');
-        await page.getByRole('textbox', { name: 'Enter your password' }).click();
-        await page.getByRole('textbox', { name: 'Enter your password' }).fill('1Passw0rd!');
-        await page.getByRole('button', { name: 'Coding Problems' }).click();
+        await this.page.getByRole('textbox', { name: 'user@example.com' }).click();
+        await this.page.getByRole('textbox', { name: 'user@example.com' }).fill('qa+100119@qaexternship.testinator.com');
+        await this.page.getByRole('textbox', { name: 'Enter your password' }).click();
+        await this.page.getByRole('textbox', { name: 'Enter your password' }).fill('1Passw0rd!');
+        await this.page.getByRole('button', { name: 'Sign In' }).click();
+        await this.page.getByRole('button', { name: 'Coding Problems' }).click();
+        await this.page.getByRole('button', { name: '×' }).click();
+        
         
     });
-        When ('The admin views the activity tab in coding problems', async function(){
+        Given ('the Admin is on the Activities tab in Coding Problems', async function(){
             //selects the activities tab
-            await page.getByRole('tab', { name: 'Activities' }).click();
+            await this.page.getByRole('tab', { name: 'Activities' }).click();
         });
-
-        Then ('the admin should see all existing activities', async function(){
-            //page should be devoice of activities therefore no locator is needed. I simply added the url
-            await expect(page).toHaveURL('https://stage.tripleten.percruit.com/admin/coding-problems');
+        
+        When('the Admin views the Activities tab in Coding Problems', async function () {
+          await expect(this.page.getByRole('heading', { name: 'Activities' })).toBeVisible();
+         })
+        Then ('the Admin should see all existing activities', async function(){
+            //page should be devoid of activities therefore no locator is needed. I simply added the url
+            await expect(this.page).toHaveURL('https://stage.tripleten.percruit.com/admin/coding-problems');
         });
 
   //sceneario selecting a category on the activities page
-        When ('the admin filters a category using the category dropdown', async function(){
-            await page.getByRole('combobox').first().click();
+        When ('the Admin filters a category using the Category dropdown', async function(){
+            await this.page.getByRole('combobox').first().click();
         });
-        Then ('the admin should be abvle to select a category from the existing category dropdown', async function(){
-            await page.getByRole('option', { name: 'TypeScript' }).click();
+        Then ('the Admin should be able to select a category from the existing Category dropdown list', async function(){
+            await this.page.getByRole('option', { name: 'TypeScript' }).click();
         });
 
 //scenario selecting a topic on activites page
-        When ('The admin wants to select a new topic from the existing topic dropdown list', async function(){
-            After('a category is already selected', async function(){
-                 await page.getByText('TypeScript').click();
-            })
+        When ('the Admin wants to select a new topic from the existing Topic dropdown list', async function(){
+            await this.page.getByRole('combobox').first().click();    
+            await this.page.getByRole('option', { name: 'TypeScript' }).click();
         })
-        Then ('The Admin should be able to select a topic from the topic dropdown list', async function(){
-            await page.getByRole('combobox').nth(1).click();
-            await page.getByRole('option', { name: 'Sample Topic' }).click();
+        When('a Category is already selected', async function(){
+            await expect(this.page.getByText('TypeScript')).toBeVisible();
+            })
+        Then ('The Admin should be able to select a topic from the Topic dropdown list', async function(){
+            await this.page.getByRole('combobox').nth(1).click();
+            await this.page.getByRole('option', { name: 'Sample Topic' }).click();
         })
 //scenario selecting a unit on activities page
-        When ('The admin wants to select a new unit from the existing unit dropdwon list', async function(){
-            After('a topic is already selected', async function (){
-                await page.getByText('Sample Topic').click();
-            })
+        When ('the Admin wants to select a new unit from the existing Unit dropdown list', async function(){
+            await this.page.getByRole('combobox').first().click();    
+            await this.page.getByRole('option', { name: 'TypeScript' }).click();
+            await this.page.getByRole('combobox').nth(1).click();
+            await this.page.getByRole('option', { name: 'Sample Topic' }).click();
+            
         })
-        Then ('The admin should be able to select a unit from the Unit dropdown list', async function(){
-            await page.getByRole('combobox').nth(2).click();
-            await page.getByRole('option', { name: 'unit' }).click();
+        When('a Topic is already selected', async function (){
+            await expect(this.page.locator('#coding-admin-tabpanel-3')).toContainText('Sample Topic');
+
+            })
+        Then ('the Admin should be able to select a unit from the Unit dropdown list', async function(){
+            await this.page.getByRole('combobox').nth(2).click();
+            await this.page.getByRole('option', { name: 'unit' }).click();
         })
 
 //scenario adding an activiy 
-        When ('the admin wants to add a new activity',async function(){
-            After('the admin selects a category from the existing category dropdwon list', async function(){
-                await page.getByRole('combobox').first().click();
-                await page.getByRole('option', { name: 'TypeScript' }).click();
-            })
-            After('the admin selects a topic from the existing corresponding Topic dropdwon list', async function(){
-                await page.getByRole('combobox').nth(1).click();
-                await page.getByRole('option', { name: 'Sample Topic' }).click();
-            })
+        When ('the Admin wants to add a new activity',async function(){
+        
         })
-        Then('the Admin selects a unit from the existing Unit dropdwon list', async function(){
-            await page.getByRole('combobox').nth(2).click();
-            await page.getByRole('option', { name: 'unit' }).click();
+        When('the Admin selects a category from the existing Category dropdown list', async function(){
+                await this.page.getByRole('combobox').first().click();
+                await this.page.getByRole('option', { name: 'TypeScript' }).click();
+            })
+        When('the Admin selects a topic from the existing corresponding Topic dropdown list', async function(){
+                await this.page.getByRole('combobox').nth(1).click();
+                await this.page.getByRole('option', { name: 'Sample Topic' }).click();
+            })
+        Then('the Admin selects a unit from the existing Unit dropdown list', async function(){
+            await this.page.getByRole('combobox').nth(2).click();
+            await this.page.getByRole('option', { name: 'unit' }).click();
         })
-        Then('the admin should be abvle to add an activity with the valid information', async function(){
+        Then('the Admin should be able to add an activity with the valid information', async function(){
 
             
-            await page.getByRole('button', { name: 'Add Activity' }).click();
-            await page.getByRole('button', { name: 'Generate Template' }).click();
-            await page.getByRole('tab', { name: 'Basic Info' }).click();
-            await page.getByRole('textbox', { name: 'Title' }).click();
-            await page.getByRole('textbox', { name: 'Title' }).fill('Test');
-            await page.getByRole('textbox', { name: 'Title' }).press('Tab');
-            await page.getByRole('textbox', { name: 'Description' }).fill('Test');
-            await page.getByRole('combobox', { name: 'Difficulty Easy' }).click();
-            await page.getByRole('option', { name: 'Easy' }).click();
-            await page.getByRole('button', { name: 'Create Problem' }).click();
+            await this.page.getByRole('button', { name: 'Add Activity' }).click();
+            await this.page.getByRole('button', { name: 'Generate Template' }).click();
+            await this.page.getByRole('tab', { name: 'Basic Info' }).click();
+            await this.page.getByRole('textbox', { name: 'Title' }).click();
+            await this.page.getByRole('textbox', { name: 'Title' }).fill('Test');
+            await this.page.getByRole('textbox', { name: 'Title' }).press('Tab');
+            await this.page.getByRole('textbox', { name: 'Description' }).fill('Test');
+            await this.page.getByRole('combobox', { name: 'Difficulty Easy' }).click();
+            await this.page.getByRole('option', { name: 'Easy' }).click();
+            await this.page.getByRole('button', { name: 'Create Problem' }).click();
             
         
-            After('the added activity should be visible to the admin on the Acitivities page', async function(){
-                await expect(page.getByRole('cell', { name: '1', exact: true })).toBeVisible();
+            Then('the added activity should be visible to the Admin on the Acitivities page', async function(){
+                await expect(this.page.getByRole('cell', { name: '1', exact: true })).toBeVisible();
             })  
         })
 
 //scenario updating an existing activity
-        When('the admin makes an edit to an existing activity with valid information', async function(){
-
-            await page.getByRole('button').filter({ hasText: /^$/ }).nth(1).click();
-            await page.getByRole('textbox', { name: 'Description' }).click();
-            await page.getByRole('textbox', { name: 'Description' }).fill('Test me like one of your french girls');
+        When('the Admin makes an edit to an existing activity with valid information', async function(){
+            await this.page.getByRole('combobox').first().click();
+            await this.page.getByRole('option', { name: 'TypeScript' }).click();
+            await this.page.getByRole('combobox').nth(1).click();
+            await this.page.getByRole('option', { name: 'Sample Topic' }).click();
+            await this.page.getByRole('combobox').nth(2).click();
+            await this.page.getByRole('option', { name: 'unit' }).click();
+            await this.page.getByRole('button').filter({ hasText: /^$/ }).nth(1).click();
+            await this.page.getByRole('tab', { name: 'Basic Info' }).click();
+            await this.page.getByRole('textbox', { name: 'Description' }).click();
+            await this.page.getByRole('textbox', { name: 'Description' }).fill('Test me like one of your french girls');
         })
-        Then('the admin should be able to save the changes in the existing activity', async function(){
-            await page.getByRole('button', { name: 'Update Problem' }).click();
-            After('the updated activity should be correctly displayed on t Activities page', async function(){
-                await expect(page.getByRole('cell', { name: 'Test me like one of your' })).toBeVisible();
+        Then('the Admin should be able to save the changes in the existing activity', async function(){
+            await this.page.getByRole('button', { name: 'Update Problem' }).click();
+           
+        })
+         Then('the updated activity should be correctly displayed on the Activities page', async function(){
+                await expect(this.page.getByRole('cell', { name: 'Test me like one of your' })).toBeVisible();
             })
-        })
 
 // scenario deletion of an existing activity    
-        When('Admin wants to delete a specific activity', async function(){
-            After('The admin selects a category from the existing Category dropdown list', async function(){
-                
-                await page.getByRole('combobox').first().click();
-                await page.getByRole('option', { name: 'TypeScript' }).click();
-            })
-            After('the admin selects a topic from the existing corresponding topic dropdown list', async function(){
-                
-                await page.getByRole('combobox').nth(1).click();
-                await page.getByRole('option', { name: 'Sample Topic' }).click();
-            })
+        When('the Admin wants to delete a specific activity', async function(){
+
         })
-        Then('the admin selects a unitt from the existing unit dropdown list', async function(){
+        When('the Admin selects a category from the existing Category dropdown list', async function(){
+                
+            await page.getByRole('combobox').first().click();
+            await page.getByRole('option', { name: 'TypeScript' }).click();
+            })
+        When('the Admin selects a topic from the existing corresponding Topic dropdown list', async function(){
+                
+            await page.getByRole('combobox').nth(1).click();
+            await page.getByRole('option', { name: 'Sample Topic' }).click();
+            })
+        
+        Then('the Admin selects a unit from the existing Unit dropdown list', async function(){
 
             await page.getByRole('combobox').nth(2).click();
             await page.getByRole('option', { name: 'unit' }).click();
@@ -130,14 +154,15 @@ declare const page: Page;
         })
 
 //scenario no units present
-        When('The admin views the activities tab', async function(){
+        When('The Admin views the Activities tab', async function(){
             await page.getByRole('tab', { name: 'Activities' }).click();
-            After('there are no existing activities', async function(){
+        
+        })
+        After('there are no existing activities', async function(){
 
             await expect(page.getByText('No activities yet. Click "Add')).toBeVisible();
-            
+
             })
-        })
         Then('the admin should not see any activites displayed on the activiites page', async function(){
             
         })
